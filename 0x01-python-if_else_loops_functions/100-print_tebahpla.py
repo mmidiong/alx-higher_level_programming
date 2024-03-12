d5d866ae53dd184bc691f3ef1e9f7e4417e50bb0
root <root@MMENYENE-IDIONG>
2024-03-12 23:24:21 +0100

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    char = i
-    if i % 2 != 0:
-        char = char - 32
-    print("{:c}".format(char), end="")
+for n in range(ord('z'), ord('a') - 1, -1):
+    if n % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(ni - diff)), end='')
