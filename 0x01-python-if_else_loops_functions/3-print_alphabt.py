be1d8917c254869a0e8703146a6004c08b05b2d3
root <root@MMENYENE-IDIONG>
2024-03-12 23:03:28 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if b != ord('e') and c != ord('q'):
-        print("{:c}".format(b), end="")
+    if c != ord('e') and c != ord('q'):
+        print("{:c}".format(c), end="")
