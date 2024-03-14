99baaaf9cbc1f899d2cc102fe42f027fab8b7762
root <root@MMENYENE-IDIONG>
2024-03-15 00:52:01 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
-    for n in range(len(name)):
-        if name[ni][0] != "_" and name[n][1] != "_":
-            print(name[n])
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
