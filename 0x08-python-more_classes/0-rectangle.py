d2a204f37a70003e3dae734550c84aa41df0f6cf
root <root@MMENYENE-IDIONG>
2024-04-08 18:29:39 +0100

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""Defines a rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This project has 10 mandatory tasks and 1 advanced task
