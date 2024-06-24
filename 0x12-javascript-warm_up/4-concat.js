e0c9458364fe05dfdbe68bbfaed6dce13bb48eea
root <root@MMENYENE-IDIONG>
2024-06-24 09:29:35 +0100

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-const process = require('process');
-let number;
-let message = 'Not a number';
-if (process.argv.length > 2) {
-  number = parseInt(process.argv[2]);
-  if (!isNaN(number)) {
-    number = String(number);
-    message = `My number: ${number}`;
-  }
-}
-console.log(message);
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(firstArg + ' is ' + secondArg);
